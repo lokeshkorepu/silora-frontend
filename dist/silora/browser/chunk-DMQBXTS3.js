8210ecd6595032bc08f02dc5f67023d139ea6183
lokesh kumar korepu <lokesh.korepu@gmail.com>
2026-02-07 02:12:28 +0530

    authentication issue solved with firebase

--- dist/silora/browser/chunk-DMQBXTS3.js
+++
@@ -1,1 +0,0 @@
-import{m as a}from"./chunk-DL53JH66.js";import"./chunk-ESGGRFI3.js";export{a as OrdersComponent};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   appName: 'Silora',
-  version: '1.0.0'
+  version: '1.0.0',
+  firebase: {
+  apiKey: "AIzaSyAgvZYJ92L3VVZbmwUCAIwUypqtHZmiNks",
+  authDomain: "silora-53f9a.firebaseapp.com",
+  projectId: "silora-53f9a",
+  storageBucket: "silora-53f9a.firebasestorage.app",
+  messagingSenderId: "906444959568",
+  appId: "1:906444959568:web:cb677a4409c8bd47e84655"
+  }
 };
